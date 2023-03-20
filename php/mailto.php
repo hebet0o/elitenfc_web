abd06d524844f54f608c601195c5d089c7d8690a
Sun Márk <suni.baboj@gmail.com>
2023-03-20 17:31:55 +0100

    mail fix

--- php/mailto.php
+++ php/mailto.php
@@ -1,21 +1,29 @@
 <?php
-if(isset($_POST['submit'])){
-    $to = "gaboriszak46@gmail.com";
-
-    $name = $_POST['name'];
-    $email= $_POST['email'];
-    $phone= $_POST['phone'];
-    $body= $_POST['body'];
-    $headers = 'From: '.$email . "\r\n";
-
-
-    $body = "name : ".$name. "\r\n" .
-    		"Phone : ".$phone. "\r\n" .
-    		"Message : " . $body;
-    if(mail($to, $body, $headers)){
-        echo "Ajánlatkérése beérkezett hozzánk! Igykeszünk minnél előbb válaszolni!";
-    }else{
-         echo "Valami hiba történt az elküldés során!";
-    }
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    die('Invalid request');
 }
-?>
+
+if (!isset($_POST['name']) || !isset($_POST['phone']) || !isset($_POST['email']) || !isset($_POST['message'])) {
+    echo "Minden mező kitöltése kötelező!";
+    exit;
+}
+
+$to = "gaboriszak46@gmail.com";
+
+$subject = "Elite NFC - Kapcsolatfelvétel";
+
+$name = $_POST['name'];
+$email = $_POST['phone'];
+$phone = $_POST['email'];
+$body = $_POST['message'];
+
+$headers = "From: {$email}" . PHP_EOL;
+$body = "Name: {$name}" . PHP_EOL .
+    "Phone: {$phone}" . PHP_EOL .
+    "Message: {$body}";
+
+if (mail($to, $subject, $body, $headers)) {
+    echo "Ajánlatkérése beérkezett hozzánk! Igykeszünk minnél előbb válaszolni!";
+} else {
+    echo "Valami hiba történt az elküldés során!";
+}
